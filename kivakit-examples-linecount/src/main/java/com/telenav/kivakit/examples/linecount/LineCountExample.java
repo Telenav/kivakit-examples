daf56ecb5bcf84f4bfc28ccc3acb21db5ce427ff
jonathan <jon@thanlocke.com>
2021-06-18 11:11:41 -0700

    ♻️ Reorganize kivakit examples

---
+++ kivakit-examples-linecount/src/main/java/com/telenav/kivakit/examples/linecount/LineCountExample.java
@@ -0,0 +1,69 @@
+package com.telenav.kivakit.examples.linecount;
+
+import com.telenav.kivakit.application.Application;
+import com.telenav.kivakit.commandline.ArgumentParser;
+import com.telenav.kivakit.commandline.SwitchParser;
+import com.telenav.kivakit.filesystem.File;
+import com.telenav.kivakit.kernel.language.values.count.Count;
+import com.telenav.kivakit.resource.project.ResourceProject;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.telenav.kivakit.commandline.SwitchParser.booleanSwitchParser;
+import static com.telenav.kivakit.filesystem.File.fileArgumentParser;
+
+/**
+ * Example application that counts the lines in its file argument. With no arguments, the application gives usage help.
+ * With the argument -show-file-size=true, it shows the size of the file in bytes.
+ *
+ * @author jonathanl (shibo)
+ */
+public class LineCountExample extends Application
+{
+    public static void main(final String[] arguments)
+    {
+        new LineCountExample().run(arguments);
+    }
+
+    private final ArgumentParser<File> INPUT =
+            fileArgumentParser("Input text file")
+                    .required()
+                    .build();
+
+    private final SwitchParser<Boolean> SHOW_FILE_SIZE =
+            booleanSwitchParser("show-file-size", "Show the file size in bytes")
+                    .optional()
+                    .defaultValue(false)
+                    .build();
+
+    private LineCountExample()
+    {
+        super(ResourceProject.get());
+    }
+
+    @Override
+    protected List<ArgumentParser<?>> argumentParsers()
+    {
+        return List.of(INPUT);
+    }
+
+    @Override
+    protected void onRun()
+    {
+        final var input = argument(INPUT);
+
+        if (get(SHOW_FILE_SIZE))
+        {
+            information("File size = $", input.sizeInBytes());
+        }
+
+        information("Lines = $", Count.count(input.reader().lines()));
+    }
+
+    @Override
+    protected Set<SwitchParser<?>> switchParsers()
+    {
+        return Set.of(SHOW_FILE_SIZE);
+    }
+}
