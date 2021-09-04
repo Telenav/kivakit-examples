a4a7bc35cc42b51a8a526ad0bf5e502cf639f77b
jonathan <jon@thanlocke.com>
2021-09-04 10:25:14 -0700

    👷 Fix deployment scripts

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,9 +13,14 @@
 
 ROOT="$(pwd)"
 BRANCH="${GITHUB_REF//refs\/heads\//}"
-SUPERPOM_BUILD="mvn --batch-mode --no-transfer-progress clean install"
+SUPERPOM_INSTALL="mvn --batch-mode --no-transfer-progress clean install"
 BUILD="mvn -Dmaven.javadoc.skip=true -DKIVAKIT_DEBUG="!Debug" -P shade -P tools --no-transfer-progress --batch-mode clean install"
 CLONE="git clone --branch "$BRANCH" --quiet"
+DEPLOY="mvn -P attach-jars -P sign-artifacts -P shade -P tools --no-transfer-progress --batch-mode -Dgpg.passphrase=${{ secrets.OSSRH_GPG_SECRET_KEY_PASSWORD }} clean deploy"
+
+if [[ "$1" == "deploy" ]]; then
+    BUILD=$DEPLOY
+fi
 
 #
 # Build kivakit
