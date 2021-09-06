e9ab1b5ec1cefb731d35efc45cf31c8430e59bde
Jonathan Locke <jon@thanlocke.com>
2021-09-05 19:31:03 -0700

    adopt superpom structure and create feature branch builds on github (#4)
    * 👷 Switch all projects to hierarchical superpom structure
    
    * 👷 Add build feature branch workflow
    
    * 👷 Add build feature branch workflow
    
    * 👷 Fix deployment scripts
    
    * 👷 Fix deployment scripts
    
    * 👷 Fix deployment scripts
    
    * 👷 Fix deployment scripts
    
    * 👷 Update build script

---
+++ .github/scripts/build.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#
+#  © 2011-2021 Telenav, Inc.
+#  Licensed under Apache License, Version 2.0
+#
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use strict;
+use warnings FATAL => 'all';
+
+#
+# Include build script from cactus-build
+#
+
+system("git clone --branch develop --quiet https://github.com/Telenav/cactus-build.git");
+
+require "./cactus-build/.github/scripts/build-include.pl";
+
+#
+# Clone repositories and build
+#
+
+my ($build_type) = @ARGV;
+my $github = "https://github.com/Telenav";
+
+clone("$github/kivakit", "dependency");
+clone("$github/kivakit-extensions", "dependency");
+clone("$github/kivakit-examples", "build");
+
+build_kivakit($build_type);
+build_kivakit_extensions($build_type);
+build_kivakit_examples($build_type);
