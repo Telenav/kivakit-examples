2c0bc15550a496fa1b9bf5d4c250ad672d2c4ae9
jonathan <jon@thanlocke.com>
2021-10-10 19:26:37 -0700

    🚧 Fix build

--- kivakit-examples-microservice/src/main/java/module-info.java
+++ kivakit-examples-microservice/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 open module kivakit.examples.microservice
 {
     // KivaKit
+    requires transitive kivakit.application;
     requires transitive kivakit.microservice;
+    requires transitive kivakit.web.swagger;
 
     // Wicket
     requires wicket.extensions;
