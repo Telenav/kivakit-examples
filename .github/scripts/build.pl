339c6fbc01ddb0e0ed9633f88e95a03a84aab23b
Jonathan Locke <jonathanl@telenav.com>
2021-10-14 09:16:15 -0700

    🚧 Add install-merged-jars.sh to github build

--- .github/scripts/build.pl
+++ .github/scripts/build.pl
@@ -26,6 +26,10 @@ my ($build_type) = @ARGV;
 my $github = "https://github.com/Telenav";
 
 clone("$github/kivakit", "dependency");
+
+system("mvn -DgroupId=com.telenav.kivakit -DartifactId=kivakit-grpc-merged -Dfile=./kivakit-extensions/kivakit-merged-jars/lib/kivakit-grpc-merged-1.1.0.jar -Dversion=1.1.0 -Dpackaging=jar install:install-file");
+system("mvn -DgroupId=com.telenav.kivakit -DartifactId=kivakit-protostuff-merged -Dfile=./kivakit-extensions/kivakit-merged-jars/lib/kivakit-protostuff-merged-1.1.0.jar -Dversion=1.1.0 -Dpackaging=jar install:install-file");
+
 clone("$github/kivakit-extensions", "dependency");
 clone("$github/kivakit-examples", "build");
 
