abefc205472077256496c3b6b0a7b9b1e158d0ea
jonathan <jon@thanlocke.com>
2021-11-29 13:27:16 -0700

    🎨 Reshaped merged jars

--- kivakit-examples-microservice/src/main/java/module-info.java
+++ kivakit-examples-microservice/src/main/java/module-info.java
@@ -3,10 +3,6 @@ open module kivakit.examples.microservice
     // KivaKit
     requires kivakit.microservice;
 
-    // Wicket
-    requires kivakit.protostuff.merged;
-    requires gson;
-
     // Module exports
     exports com.telenav.kivakit.examples.microservice;
 }
