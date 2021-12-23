1db6f4dcb79315f3a9a0fc006cd7553f2da8dfb3
jonathan <jon@thanlocke.com>
2021-12-23 13:20:24 -0700

    👷 Prepare for 1.2.1

--- kivakit-examples-lambda/src/main/java/com/telenav/kivakit/examples/lambda/DivisionMicroservice.java
+++ kivakit-examples-lambda/src/main/java/com/telenav/kivakit/examples/lambda/DivisionMicroservice.java
@@ -1,13 +1,9 @@
 package com.telenav.kivakit.examples.lambda;
 
-import com.telenav.kivakit.examples.lambda.requests.DivisionRequest;
 import com.telenav.kivakit.kernel.language.values.version.Version;
 import com.telenav.kivakit.microservice.Microservice;
 import com.telenav.kivakit.microservice.MicroserviceMetadata;
-import com.telenav.kivakit.microservice.microservlet.MicroservletRequest;
 import com.telenav.kivakit.microservice.protocols.lambda.MicroserviceLambdaService;
-
-import java.util.Set;
 
 public class DivisionMicroservice extends Microservice<Void>
 {
