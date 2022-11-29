9e0d06b4be07d5154abd1ec76d03613c3dcce002
Jonathan Locke <jonathanl@telenav.com>
2022-11-29 12:07:15 -0700

    Bugfix/demodularize (#31)
    * 🚧 work in progress on demodularizing kivakit
    
    * 🐛 Fix remaining tests
    
    * 👷 Update javadoc paths for antlr4

--- kivakit-examples-application/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module kivakit.examples.application
-{
-    // KivaKit
-    requires kivakit.application;
-
-    // Module exports
-    exports com.telenav.kivakit.examples.application;
-}
--- kivakit-examples-settings/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module kivakit.examples.settings
-{
-    // KivaKit
-    requires kivakit.application;
-
-    // Module exports
-    exports com.telenav.kivakit.examples.settings.lookup;
-    exports com.telenav.kivakit.examples.settings.simple;
-}
