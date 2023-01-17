6783498291fcbcad55d9cf6fe846efd0ad8bcb13
jonathanl-telenav <jonathanl@telenav.com>
2023-01-17 09:52:03 -0700

    🎨 api cleanups  - move nested converters to top level  - provide default constructors for converters that throw

--- kivakit-snippets/src/main/java/com/telenav/kivakit/snippets/Snippet14.java
+++ kivakit-snippets/src/main/java/com/telenav/kivakit/snippets/Snippet14.java
@@ -1,7 +1,6 @@
 package com.telenav.kivakit.snippets;
 
-import com.telenav.kivakit.conversion.core.time.HumanizedLocalDateTimeConverter;
-import com.telenav.kivakit.core.registry.RegistryTrait;
+import com.telenav.kivakit.conversion.core.time.kivakit.HumanizedKivaKitLocalDateTimeConverter;
 
 import static com.telenav.kivakit.core.time.LocalTime.now;
 
@@ -15,7 +14,7 @@ public class Snippet14 extends BaseSnippet
     @Override
     protected Object onSnippet()
     {
-        var converter = new HumanizedLocalDateTimeConverter(this);
+        var converter = new HumanizedKivaKitLocalDateTimeConverter(this);
         return converter.unconvert(now().asLocalTime())
             + ", "
             + converter.convert("Today 6.00pm");
--- kivakit-snippets/src/main/java/com/telenav/kivakit/snippets/Snippet16.java
+++ kivakit-snippets/src/main/java/com/telenav/kivakit/snippets/Snippet16.java
@@ -1,13 +1,10 @@
 package com.telenav.kivakit.snippets;
 
 import com.telenav.kivakit.application.Application;
-import com.telenav.kivakit.core.object.Lazy;
 import com.telenav.kivakit.core.os.ConsoleTrait;
-import com.telenav.kivakit.serialization.gson.factory.KivaKitCoreGsonFactory;
 
 import static com.telenav.kivakit.core.collections.list.StringList.split;
-import static com.telenav.kivakit.core.object.Lazy.lazy;
-import static com.telenav.kivakit.network.http.secure.SecureHttpNetworkLocation.secureHttpNetworkLocation;
+import static com.telenav.kivakit.network.https.HttpsNetworkLocation.secureHttpNetworkLocation;
 
 public class Snippet16 extends Application implements ConsoleTrait
 {
